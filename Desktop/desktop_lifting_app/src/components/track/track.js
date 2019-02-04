d612530447e2866e5bde64d79296084fbb6d8e4b
Clayton Kingsbury <ckingsbury2@gmail.com>
2019-02-04 10:03:15 -0700

    back up

--- Desktop/desktop_lifting_app/src/components/track/track.js
+++ Desktop/desktop_lifting_app/src/components/track/track.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid.css';
+import './ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+
+
 
 const ColoredLine = ({ color }) => (
     <hr
@@ -14,47 +16,80 @@ const ColoredLine = ({ color }) => (
     />
 );
 
+var React1 = require('react');
+
+var buttonStyle = {
+  margin: '10px 10px 10px 0'
+};
+
+var Button = React1.createClass({
+  render: function () {
+    return (
+      <button
+        className="btn btn-default"
+        style={buttonStyle}
+        onClick={this.props.handleClick}>{this.props.label}</button>
+    );
+  }
+});
+
 class track extends React.Component {
   constructor(props) {
         super(props);
 
         this.state = {
             columnDefs: [
-                {headerName: "Exercise", field: "exercise"},
-                {headerName: "Sets", field: "sets"},
-                {headerName: "Reps", field: "reps"},
-                {headerName: "Weight", field: "weight"},
-                {headerName: "Effort", field: "effort"}
+                {headerName: "Exercise", field: "exercise", editable: true},
+                {headerName: "Sets", field: "sets", editable: true},
+                {headerName: "Reps", field: "reps", editable: true},
+                {headerName: "Weight", field: "weight", editable: true},
+                {headerName: "Effort", field: "effort", editable: true}
 
             ],
             rowData: [
-                {exercise: "Bench", sets: 5, reps: 5, weight: 275, effort: "medium"},
-                {exercise: "DB Bench", sets: 3, reps: 10, weight: 100, effort: "medium"},
-                {exercise: "Fly ", sets: 3, reps: 12, weight: 45, effort: "medium"},
-                {exercise: "Row", sets: 3, reps: 10, weight: 225, effort: "medium"}
+              {exercise: "Bench", sets: 5, reps: 5, weight: 275, effort: "medium"},
+              {exercise: "DB Bench", sets: 3, reps: 10, weight: 100, effort: "medium"},
+              {exercise: "Fly ", sets: 3, reps: 12, weight: 45, effort: "medium"},
+              {exercise: "Row", sets: 3, reps: 10, weight: 225, effort: "medium"}
             ]
         }
+        var buttomAdd = document.createElement("Button");
     }
+    // componentDidMount() { //https://medium.com/ag-grid/get-started-with-react-grid-in-5-minutes-f6e5fb16afa
+    //     fetch('https://api.myjson.com/bins/15psn9')
+    //         .then(result => result.json())
+    //         .then(rowData => this.setState({rowData}))
+    // }
+
+
+
 	render() {
 		return (
+      <div>
 			<div className="text-left">
 				<p> accountName	 accoutUserName </p>
 
 				<ColoredLine color="blue" />
 				<h2>Enter data to be tracked</h2>
-			</div>,
+			</div>
 
       <div
                       className="ag-theme-balham"
-                      style={{ height: '200px', width: '600px' }}
+                      style={{ height: '80%', width: '80%', padding: "50px" }}
                   >
                       <AgGridReact
+                          enableSorting={true}
                           columnDefs={this.state.columnDefs}
                           rowData={this.state.rowData}>
                       </AgGridReact>
                   </div>
+                  <button
+                          className="btn btn-default"
+                          style={buttonStyle}
+                          onClick={this.props.handleClick}>{this.props.label}</button>
+                  </div>
     );
   }
 }
-
+track.exports = Button;
 export default track
